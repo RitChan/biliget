f7fe141eead22bf26dcb7dadcf081c573c9f402e
chenh <chenh0510@qq.com>
2024-06-16 01:49:38 +0800

    refactor: redesign api

--- config/config.go
+++ config/config.go
@@ -14,11 +14,12 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path"
 )
 
 type BiligetConfig struct {
-	Filepath    string         // Manual reflection :)
-	CookieCache []*http.Cookie `json:"cookie_cache"`
+	DownloadDirectory string         `json:"download_directory"`
+	CookieCache       []*http.Cookie `json:"cookie_cache"`
 }
 
 var global *BiligetConfig
@@ -30,12 +31,12 @@ func Global() *BiligetConfig {
 	return global
 }
 
-func Dump(filepath string) error {
-	if global == nil {
-		log.Println("Warning: global config is nil, no file saved")
+func Dump(config *BiligetConfig, filepath string) error {
+	if config == nil {
+		log.Println("Warning: config is nil, no file saved")
 		return nil
 	}
-	bytes, err := json.Marshal(Global())
+	bytes, err := json.Marshal(config)
 	if err != nil {
 		return err
 	}
@@ -46,19 +47,37 @@ func Dump(filepath string) error {
 	return nil
 }
 
+func DefaultPath() (string, error) {
+	p := userConfigPath()
+	if p != "" {
+		return p, nil
+	}
+	p, err := appConfigPath()
+	if err != nil {
+		return "", err
+	}
+	return p, nil
+}
+
+// Tolerate any error
 func initConfig() *BiligetConfig {
 	global = new(BiligetConfig)
-	err := loadFile(global, getConfigPath())
+	setDefault(global)
+	p, err := DefaultPath()
 	if err != nil {
-		log.Printf("Warning: %s\n", err.Error())
+		return global
+	}
+	err = loadFile(global, p)
+	if err != nil {
 		setDefault(global)
 	}
 	return global
 }
 
+// Ensure all fields are set
 func setDefault(config *BiligetConfig) {
 	config.CookieCache = make([]*http.Cookie, 0)
-	config.Filepath = ""
+	config.DownloadDirectory = ""
 }
 
 func loadFile(config *BiligetConfig, filepath string) error {
@@ -70,11 +89,10 @@ func loadFile(config *BiligetConfig, filepath string) error {
 	if err != nil {
 		return err
 	}
-	config.Filepath = filepath
 	return nil
 }
 
-func getConfigPath() string {
+func userConfigPath() string {
 	// CLI
 	argv := os.Args[1:]
 	configFlag := false
@@ -91,6 +109,13 @@ func getConfigPath() string {
 	if configPath != "" {
 		return configPath
 	}
-	// Default
-	return "biliget.json"
+	return ""
+}
+
+func appConfigPath() (string, error) {
+	p, err := os.UserConfigDir()
+	if err != nil {
+		return "", err
+	}
+	return path.Join(p, "biliget", "biliget.json"), nil
 }
